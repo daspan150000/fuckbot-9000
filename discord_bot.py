f4430b4d6591c415060f2bbe0567055d51e48237
david <davidmerlin123@gmail.com>
2020-12-10 14:38:19 +0100

    building bot

--- discord_bot.py
+++ discord_bot.py
@@ -28,12 +28,13 @@ async def version(context):
 
 @client.command(name = "hjælp")
 async def hjælp(context):
-    com_embed = discord.Embed(title = "kommandoer", description = "disse er kommandoerne du kan bruge med fuckbot-9000", color = 0xFF0000)
-    com_embed.set_author(name = "fuckbot-9000")
-    com_embed.add_field(name = "--version", value = "viser hvilken version botten er i" , inline = False)
-    com_embed.add_field(name = "--slet_besked", value = "sletter bottens sidste 100 beskeder. virker godt mod spam" , inline = False)
-    com_embed.add_field(name = "botten vil også reagere på visse ord og sætninger.", value = "'gamer' er en af ordene botten reagere på \n'fuck dig thomas' er en sætning botten vil reagere på" , inline = False)
-    com_embed.add_field(name = "det er ikke vigtigt om du skriver disse sætniger med stort eller småt", value = "så slå dig løs. og fuck thomas." , inline = False)
+    if not context.author.client:
+        com_embed = discord.Embed(title = "kommandoer", description = "disse er kommandoerne du kan bruge med fuckbot-9000", color = 0xFF0000)
+        com_embed.set_author(name = "fuckbot-9000")
+        com_embed.add_field(name = "--version", value = "viser hvilken version botten er i" , inline = False)
+        com_embed.add_field(name = "--slet_besked", value = "sletter bottens sidste 100 beskeder. virker godt mod spam" , inline = False)
+        com_embed.add_field(name = "botten vil også reagere på visse ord og sætninger.", value = "'gamer' er en af ordene botten reagere på \n'fuck dig thomas' er en sætning botten vil reagere på" , inline = False)
+        com_embed.add_field(name = "det er ikke vigtigt om du skriver disse sætniger med stort eller småt", value = "så slå dig løs. og fuck thomas." , inline = False)
     await context.message.channel.send(embed = com_embed)
 
 
@@ -47,32 +48,36 @@ async def slet_besked(context):
 
 @client.event
 async def on_ready():
-    seconds = time.time()
-    local_time = time.localtime(seconds)
-    current_time = str(local_time.tm_hour + 1) + " " + str(local_time.tm_min - 2) + " " + str(local_time.tm_sec)
-    print(current_time)
-    await client.change_presence(status= discord.Status.do_not_disturb, activity = discord.Game(name = "pik"))
+    print("fuckbot-9000 is logged in")
+    await client.change_presence(status= discord.Status.do_not_disturb, activity = discord.Game(name = "kiks med thomas' far"))
 
- 
-    
-        
+#seconds = time.time()
+#local_time = time.localtime(seconds)
+#current_time = str(local_time.tm_hour + 1) + ":" + str(local_time.tm_min - 2) + ":" + str(local_time.tm_sec - 10)
+#print(current_time)
+#if current_time == "15:00:00":
+#    await client.change_presence(status= discord.Status.do_not_disturb, activity = discord.Game(name = "kiks med thomas' far"))
+#if current_time == 
+
+
 
 @client.event
 async def on_message(message):
-    File = open("fuck.txt", "r")
-    read = File.read()
-    File.close()
-    if message.content.lower() in read:
-        answer = "ja FUCK dig thomas"
-        general_channel = client.get_channel(671001377899806783)
-        await general_channel.send(answer)
-    await client.process_commands(message)
+    if not message.author.client:
+        File = open("fuck.txt", "r")
+        read = File.read()
+        File.close()
+        if message.content.lower() in read:
+            answer = "ja FUCK dig thomas"
+            general_channel = client.get_channel(671001377899806783)
+            await general_channel.send(answer)
+        await client.process_commands(message)
 
-    if message.content.lower() == "gamer":
-        åh_gamer = "århh GAMER!"
-        general_channel = client.get_channel(671001377899806783)
-        await general_channel.send(åh_gamer)
-    await client.process_commands(message)
+        if message.content.lower() == "gamer":
+            åh_gamer = "århh GAMER!"
+            general_channel = client.get_channel(671001377899806783)
+            await general_channel.send(åh_gamer)
+        await client.process_commands(message)
 
 #671001377899806783 fuck dig thomas
 
