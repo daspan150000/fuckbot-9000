2f04448346f18d0245066dbd529aa0c3b000a5bc
david <davidmerlin123@gmail.com>
2020-12-14 21:43:52 +0100

    building bot

--- discord_bot.py
+++ discord_bot.py
@@ -1,33 +1,35 @@
-# program version "1.0.0"
+# program version "1.0.1"
 # social ovalen id: 671001377899806783
 
 
 
 import discord
-from discord.ext import commands
+from discord.ext import commands, tasks
 import os
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 import asyncio
 import time
 import random
+import requests
 
 
 
 
-#client
+
+#client med "--" som prefix
 client = commands.Bot(command_prefix = "--")
 
+
+#hvis nogen skriver "version" i (social-ovalen)
 @client.command(name="version")
 async def version(context):
     
     myEmbed = discord.Embed(title = "current version", description = "the bot is in version 1.0", color = 0x00ff00)
     myEmbed.set_author(name = "fuckbot-9000")
-    myEmbed.add_field(name = "code version", value = "v1.0.0", inline = False)
+    myEmbed.add_field(name = "code version", value = "v1.0.1", inline = False)
     myEmbed.add_field(name = "Date Released:", value = "8-12-2020", inline = False)
     await context.message.channel.send(embed = myEmbed)
 
-
+#hvis nogen skriver "hjælp" i (social-ovalen)
 @client.command(name = "hjælp")
 async def hjælp(context):
     if not context.author.client:
@@ -39,82 +41,87 @@ async def hjælp(context):
         com_embed.add_field(name = "det er ikke vigtigt om du skriver disse sætniger med stort eller småt", value = "så slå dig løs. og fuck thomas." , inline = False)
     await context.message.channel.send(embed = com_embed)
 
-
+#hjælper funktion
 def is_me(m):
     return m.author == client.user
 
+#slet beskeder
 @client.command(name="slet_besked", aliases =["slet", "slet_beskeder"])
 @commands.has_permissions(manage_messages = True)
 async def slet_besked(ctx, arg1:int):
     print(arg1)
     await ctx.channel.purge(limit = arg1, check = is_me)
 
-
+#når der sker en fejl
 @client.event
 async def on_command_error(context, error):
     print(context.command.name + " was invoked incorrectly")
     print(error)
 
 
-
+#når robotten er klar
 @client.event
 async def on_ready():
     print("fuckbot-9000 is logged in")
+    response = requests.get("https://evilinsult.com/generate_insult.php?lang=en&type=json")
+    insult = response.json("insult")
+    print(insult)
     
-    
-
-#seconds = time.time()
-#local_time = time.localtime(seconds)
-#current_time = str(local_time.tm_hour + 1) + ":" + str(local_time.tm_min - 2) + ":" + str(local_time.tm_sec - 10)
-#print(current_time)
-#if current_time == "15:00:00":
-#    await client.change_presence(status= discord.Status.do_not_disturb, activity = discord.Game(name = "kiks med thomas' far"))
-#if current_time == 
 
 
-
-
+#når (social-ovalen) får en besked
 @client.event
 async def on_message(message):
-    File = open("fuck.txt", "r")
+    File = open("fuck", "r")
     read = File.read()
     File.close()
+    #hvis den besked er "fuck dig thomas", så skal botten svarer "ja FUCK dig thomas"
     if message.content.lower() in read:
         answer = "ja FUCK dig thomas"
         general_channel = client.get_channel(671001377899806783)
         await general_channel.send(answer)
     await client.process_commands(message)
 
+    #hvis beskeden er "gamer", så skal botten svarer "århh GAMER!"
     if message.content.lower() == "gamer":
         åh_gamer = "århh GAMER!"
         general_channel = client.get_channel(671001377899806783)
         await general_channel.send(åh_gamer)
     await client.process_commands(message)
-
     
     fil = open("læg_dig", "r")
     læs = fil.read()
     fil.close()
+    #hvis beskeden er "læg dig ned thomas", så skal botten svarer "Ja læg dig HELT ned og sig undskyld Thomas"
     if message.content.lower() in læs:
         læg_dig = "Ja læg dig HELT ned og sig undskyld Thomas"
         general_channel = client.get_channel(671001377899806783)
         await general_channel.send(læg_dig)
     await client.process_commands(message)
 
+
+
+        
+
+
+#skift aktivitet en gang hver halve time
 async def ch_pr():
     await client.wait_until_ready()
 
+    #aktiviteter der starter med "spiller"
     activities = ["kiks med thomas", "pik", "baseball med thomas løg"]
 
     while not client.is_closed():
-
+        #vælg en tilfældig aktivitet
         status = random.choice(activities)
-
+        #skft aktivitet
         await client.change_presence(status= discord.Status.do_not_disturb, activity = discord.Game(name = status))
-
+        #vent en halv time
         await asyncio.sleep(1800)
 
 client.loop.create_task(ch_pr())
+
+
 #671001377899806783 fuck dig thomas
 
 
