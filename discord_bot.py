366f8d1983b26e5fe891cad0b9775cc131088309
david <davidmerlin123@gmail.com>
2020-12-16 19:07:41 +0100

    building bot

--- discord_bot.py
+++ discord_bot.py
@@ -3,7 +3,7 @@
 
 
 #husk at skrive på requirements
-import discord
+import discord 
 from discord.ext import commands, tasks
 import os
 import asyncio
@@ -25,26 +25,26 @@ async def on_ready():
 
 
 #hvis nogen skriver "version" i (social-ovalen)
-@client.command(name="version")
-async def version(context):
-    
-    myEmbed = discord.Embed(title = "current version", description = "the bot is in version 1.0", color = 0x00ff00)
-    myEmbed.set_author(name = "fuckbot-9000")
-    myEmbed.add_field(name = "code version", value = "v1.0.1", inline = False)
-    myEmbed.add_field(name = "Date Released:", value = "8-12-2020", inline = False)
-    await context.message.channel.send(embed = myEmbed)
+#@client.command(name="version")
+#async def version(context):
+#    
+#    myEmbed = discord.Embed(title = "current version", description = "the bot is in version 1.0", color = 0x00ff00)
+#    myEmbed.set_author(name = "fuckbot-9000")
+#    myEmbed.add_field(name = "code version", value = "v1.0.1", inline = False)
+#    myEmbed.add_field(name = "Date Released:", value = "8-12-2020", inline = False)
+#    await context.message.channel.send(embed = myEmbed)
 
 #hvis nogen skriver "hjælp" i (social-ovalen)
-@client.command(name = "hjælp")
-async def hjælp(context):
-    if not context.author.client:
-        com_embed = discord.Embed(title = "kommandoer", description = "disse er kommandoerne du kan bruge med fuckbot-9000", color = 0xFF0000)
-        com_embed.set_author(name = "fuckbot-9000")
-        com_embed.add_field(name = "--version", value = "viser hvilken version botten er i" , inline = False)
-        com_embed.add_field(name = "--slet_besked", value = "sletter bottens sidste 100 beskeder. virker godt mod spam" , inline = False)
-        com_embed.add_field(name = "botten vil også reagere på visse ord og sætninger.", value = "'gamer' er en af ordene botten reagere på \n'fuck dig thomas' er en sætning botten vil reagere på \n'læg dig thomas' kan også bruges for virkelig at vise thomas hvor han høre til." , inline = False)
-        com_embed.add_field(name = "det er ikke vigtigt om du skriver disse sætniger med stort eller småt", value = "så slå dig løs. og fuck thomas." , inline = False)
-    await context.message.channel.send(embed = com_embed)
+#@client.command(name = "hjælp")
+#async def hjælp(context):
+#    if not context.author.client:
+#        com_embed = discord.Embed(title = "kommandoer", description = "disse er kommandoerne du kan bruge med fuckbot-9000", color = 0xFF0000)
+#        com_embed.set_author(name = "fuckbot-9000")
+#        com_embed.add_field(name = "--version", value = "viser hvilken version botten er i" , inline = False)
+#        com_embed.add_field(name = "--slet_besked", value = "sletter bottens sidste 100 beskeder. virker godt mod spam" , inline = False)
+#        com_embed.add_field(name = "botten vil også reagere på visse ord og sætninger.", value = "'gamer' er en af ordene botten reagere på \n'fuck dig thomas' er en sætning botten vil reagere på \n'læg dig thomas' kan også bruges for virkelig at vise thomas hvor han høre til." , inline = False)
+#        com_embed.add_field(name = "det er ikke vigtigt om du skriver disse sætniger med stort eller småt", value = "så slå dig løs. og fuck thomas." , inline = False)
+#    await context.message.channel.send(embed = com_embed)
 
 
 
@@ -70,19 +70,19 @@ async def on_command_error(context, error):
     print(error)
 
 
-@client.command(name = "roast")
-async def roast(context, person):
-    if context.author != client.user:
-        insults = open("insults.txt", "r")
-        lines = insults.readlines()
-        print(len(lines))
-        insults.close()
-        rand = random.randint(0, len(lines))
-        print(rand)
-        final_roast = str(person) + ", " + str(lines[rand])
-        print(final_roast)
-        general_channel = client.get_channel(context.channel.id)
-        await general_channel.send(final_roast)
+#@client.command(name = "roast")
+#async def roast(context, person):
+#    if context.author != client.user:
+#        insults = open("insults.txt", "r")
+#        lines = insults.readlines()
+#        print(len(lines))
+#        insults.close()
+#        rand = random.randint(0, len(lines))
+#        print(rand)
+#        final_roast = str(person) + ", " + str(lines[rand])
+#        print(final_roast)
+#        general_channel = client.get_channel(context.channel.id)
+#        await general_channel.send(final_roast)
     
 #671001377899806783
 
