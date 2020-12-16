c9bdaa5e232374b95bf20adcc3f8f105ffcb064d
david <davidmerlin123@gmail.com>
2020-12-16 22:00:46 +0100

    building bot

--- discord_bot.py
+++ discord_bot.py
@@ -89,7 +89,7 @@ async def on_message(message):
         context_chat = client.get_channel(message.channel.id)
         await context_chat.send(embed = com_embed)
 
-    if message.get_channel() == "785602392288788480"
+    if message.get_channel() == "785602392288788480":
         if message.content.lower() == "--meme":
             reddit = praw.Reddit(client_id = os.environ["reddit_client_id"],
             client_secret = os.environ["reddit_client_secret"],
