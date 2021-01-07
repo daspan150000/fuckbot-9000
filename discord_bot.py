e4b2cf81a4120519e52ee411fda50cfb7455c656
david <davidmerlin123@gmail.com>
2021-01-07 01:00:11 +0100

    building bot

--- discord_bot.py
+++ discord_bot.py
@@ -89,16 +89,25 @@ async def on_message(message):
 
     
     if message.content.lower() == "--meme":
-        images = []
-        for fil in os.walk("memes"):
-            if fil.endswith(".jpg"):
-                images.append(fil)
-        rand = random.choice(images)
-        image = images[rand]
-        discord_file = discord.File(image)
+        reddit = praw.Reddit(client_id = os.environ["reddit_client_id"],
+        client_secret = os.environ["reddit_client_secret"],
+        username = "daspan15000",
+        password = os.environ["pass"],
+        user_agent = "fuckbotpraw")
+
+        subreddit = reddit.subreddit("DankMemes")
+        all_meme_subs = []
+        top_memes = subreddit.top(limit = 50)
+        for meme_submission in top_memes:
+            all_meme_subs.append(meme_submission)
+
+        random_meme_sub = random.choice(all_meme_subs)
+        meme_name = random_meme_sub.title
+        meme_url = random_meme_sub.url
+        meme_em = discord.Embed(title = meme_name)
+        meme_em.set_image(url = meme_url)
         context_channel = client.get_channel(message.channel.id)
-        os.remove(fil)
-        await context_channel.send(file = discord_file)
+        await context_channel.send(embed = meme_em)
 
 
 
